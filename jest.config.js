211ce286667b89bfbced42b886002754c4747901
AnasMemon18 <anasmemon7864@gmail.com>
2026-09-18 17:02:49 +0530

    fix(test): correct jest config option and include jest types in tsconfig

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }],
+  },
+};
